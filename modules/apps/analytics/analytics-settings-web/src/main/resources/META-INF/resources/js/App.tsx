2c5397217025a040b65aaff3e9e1c18e449fc481
Marcio Fonseca <marcio.fonseca@liferay.com>
2022-12-07 19:17:47 -0300

    LRAC-12387 set const to be exported

--- modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/App.tsx
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/App.tsx
@@ -47,7 +47,7 @@ const initialState = {
 	token: '',
 };
 
-const AppContextData = React.createContext<TData>(initialState);
+export const AppContextData = React.createContext<TData>(initialState);
 const AppContextDispatch = React.createContext<any>(null);
 
 const useData = () => useContext(AppContextData);
