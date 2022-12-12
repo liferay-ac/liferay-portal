0cb58bbe4167a2a45e0bcc92d0dff3d98416e266
Adriano Interaminense <adriano.interaminense@gmail.com>
2022-12-12 15:37:22 -0300

    LRAC-12485 Update unit tests

--- modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/properties/Properties.spec.tsx
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/properties/Properties.spec.tsx
@@ -15,7 +15,7 @@
 import fetch from 'jest-fetch-mock';
 
 import '@testing-library/jest-dom/extend-expect';
-import {render, screen} from '@testing-library/react';
+import {act, fireEvent, render, screen, within} from '@testing-library/react';
 import React from 'react';
 
 import {loadingElement} from '../../utils/tests/helpers';
@@ -27,12 +27,34 @@ describe('Properties', () => {
 		jest.restoreAllMocks();
 	});
 
-	it('renders without crashing', async () => {
+	it('renders properties table header', async () => {
 		fetch.mockResponseOnce(JSON.stringify(fetchPropertiesResponse));
 
 		render(<Properties />);
 
 		await loadingElement();
+
+		expect(screen.getAllByRole('columnheader')).toHaveLength(5);
+		expect(
+			screen.getByRole('columnheader', {
+				name: /available-properties/i,
+			})
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole('columnheader', {
+				name: /channels/i,
+			})
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole('columnheader', {
+				name: /sites/i,
+			})
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole('columnheader', {
+				name: /commerce/i,
+			})
+		).toBeInTheDocument();
 	});
 
 	it('renders properties table content', async () => {
@@ -42,30 +64,21 @@ describe('Properties', () => {
 
 		await loadingElement();
 
-		expect(screen.getByTestId(/Liferay DXP/i)).toBeInTheDocument();
-		expect(screen.getByText(/Liferay DXP/i)).toBeInTheDocument();
+		const first = screen.getByTestId(/Liferay DXP/i);
 
-		const firstTableColumn = screen.getAllByRole('591043793166298694');
+		expect(within(first).getByText(/Liferay DXP/i)).toBeInTheDocument();
+		expect(within(first).getByText(/-/i)).toBeInTheDocument();
+		expect(within(first).getByText(/0/i)).toBeInTheDocument();
+		expect(within(first).getByRole('toggle-switch')).toBeInTheDocument();
+		expect(within(first).getByRole('assign-button')).toBeInTheDocument();
 
-		expect(firstTableColumn).toHaveLength(5);
-		expect(firstTableColumn[0]).toHaveTextContent(/Liferay DXP/i);
-		expect(firstTableColumn[1]).toHaveTextContent(/-/i);
-		expect(firstTableColumn[2]).toHaveTextContent(/0/i);
-		expect(firstTableColumn[3].firstChild).toHaveClass(
-			'toggle-switch simple-toggle-switch'
-		);
-		expect(firstTableColumn[4]).toHaveTextContent(/assign/i);
+		const second = screen.getByTestId(/Beryl Commerce/i);
 
-		const secondTableColumn = screen.getAllByRole('507692450375472147');
-
-		expect(secondTableColumn).toHaveLength(5);
-		expect(secondTableColumn[0]).toHaveTextContent(/Beryl Commerce/i);
-		expect(secondTableColumn[1]).toHaveTextContent(/-/i);
-		expect(secondTableColumn[2]).toHaveTextContent(/5/i);
-		expect(secondTableColumn[3].firstChild).toHaveClass(
-			'toggle-switch simple-toggle-switch'
-		);
-		expect(secondTableColumn[4]).toHaveTextContent(/assign/i);
+		expect(within(second).getByText(/Beryl Commerce/i)).toBeInTheDocument();
+		expect(within(second).getByText(/-/i)).toBeInTheDocument();
+		expect(within(second).getByText(/5/i)).toBeInTheDocument();
+		expect(within(second).getByRole('toggle-switch')).toBeInTheDocument();
+		expect(within(second).getByRole('assign-button')).toBeInTheDocument();
 	});
 
 	it('renders filter options', async () => {
@@ -75,7 +88,98 @@ describe('Properties', () => {
 
 		await loadingElement();
 
-		expect(screen.getAllByText('available-properties')).toHaveLength(2);
-		expect(screen.getByText('create-date')).toBeInTheDocument();
+		fireEvent.click(
+			screen.getByRole('button', {
+				name: /filter-and-order/i,
+			})
+		);
+
+		expect(
+			screen.getByRole('menuitem', {
+				name: /available-properties/i,
+			})
+		).toBeInTheDocument();
+		expect(
+			screen.getByRole('menuitem', {
+				name: /create-date/i,
+			})
+		).toBeInTheDocument();
+	});
+
+	it('renders assign modal', async () => {
+		fetch.mockResponseOnce(JSON.stringify(fetchPropertiesResponse));
+
+		jest.useFakeTimers();
+
+		render(<Properties />);
+
+		await loadingElement();
+
+		expect(document.body).not.toHaveClass('modal-open');
+
+		fireEvent.click(screen.getAllByRole(/assign-button/i)[0]);
+
+		expect(document.body).toHaveClass('modal-open');
+	});
+
+	it('enable commerce in the first table column', async () => {
+		fetch.mockResponse(JSON.stringify(fetchPropertiesResponse));
+
+		render(<Properties />);
+
+		await loadingElement();
+
+		const firstRow = screen.getByTestId(/Liferay DXP/i);
+
+		const toggleSwitch = within(firstRow).getByRole('toggle-switch');
+
+		expect(toggleSwitch).not.toBeChecked();
+		expect(within(firstRow).getByText(/-/i)).toBeInTheDocument();
+
+		await act(async () => {
+			await fireEvent.click(toggleSwitch);
+		});
+
+		fetch.mockResponse(
+			JSON.stringify({
+				...fetchPropertiesResponse,
+				items: [
+					{
+						...fetchPropertiesResponse.items[0],
+						commerceSyncEnabled: true,
+					},
+					fetchPropertiesResponse.items[1],
+				],
+			})
+		);
+
+		expect(toggleSwitch).toBeChecked();
+		expect(within(firstRow).getAllByText(/0/i)[0]).toBeInTheDocument();
+	});
+
+	it('renders modal to create a new property', async () => {
+		fetch.mockResponse(JSON.stringify(fetchPropertiesResponse));
+
+		jest.useFakeTimers();
+
+		render(<Properties />);
+
+		await loadingElement();
+
+		const addProperyButton = screen.getByRole('button', {
+			name: /create-new-property/i,
+		});
+
+		expect(addProperyButton).toBeInTheDocument();
+		expect(document.body).not.toHaveClass('modal-open');
+
+		await act(async () => {
+			await fireEvent.click(addProperyButton);
+
+			jest.runAllTimers();
+		});
+
+		expect(document.body).toHaveClass('modal-open');
+		expect(screen.getByText(/new-property/i)).toBeInTheDocument();
 	});
 });
