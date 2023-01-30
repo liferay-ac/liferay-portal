205f5d4637be79de3828ead554f7a63997e46e6b
Marcos Martins <marcos.martins@liferay.com>
2023-01-30 16:19:32 -0300

    LRAC-12833 export user according to feature flag

--- modules/apps/analytics/analytics-settings-impl/src/main/java/com/liferay/analytics/settings/internal/configuration/AnalyticsConfigurationRegistryImpl.java
+++ modules/apps/analytics/analytics-settings-impl/src/main/java/com/liferay/analytics/settings/internal/configuration/AnalyticsConfigurationRegistryImpl.java
@@ -603,9 +603,18 @@ public class AnalyticsConfigurationRegistryImpl
 						companyId,
 						refreshDispatchTriggerNames.toArray(new String[0]));
 
-					_analyticsDXPEntityBatchExporter.export(
-						companyId,
-						refreshDispatchTriggerNames.toArray(new String[0]));
+					if (GetterUtil.getBoolean(
+							PropsUtil.get("feature.flag.LRAC-10632")) &&
+						!GetterUtil.getBoolean(
+							PropsUtil.get("feature.flag.LRAC-10757"))) {
+
+						_analyticsDXPEntityBatchExporter.export(
+							companyId,
+							new String[] {
+								AnalyticsDXPEntityBatchExporterConstants.
+									USER_DISPATCH_TRIGGER_NAME
+							});
+					}
 				}
 
 				if (!unscheduleDispatchTriggerNames.isEmpty()) {
