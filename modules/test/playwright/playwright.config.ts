2ccb190a8b3fc355b34a9e945e8aee2d9117eeea
Marcos Martins <marcos.martins@liferay.com>
2024-02-02 10:45:24 -0300

    LPD-16508 register test suites

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -5,6 +5,7 @@
 
 import {defineConfig} from '@playwright/test';
 
+import {config as analyticsSettingsWebConfig} from './tests/analytics-settings-web/config';
 import {config as batchPlannerConfig} from './tests/batch-planner/config';
 import {config as clientExtensionWebConfig} from './tests/client-extension-web/config';
 import {config as commerceConfig} from './tests/commerce/config';
@@ -18,6 +19,7 @@ import {config as layoutAdminWebConfig} from './tests/layout-admin-web/config';
 import {config as layoutContentPageEditorWebConfig} from './tests/layout-content-page-editor-web/config';
 import {config as lockedItemsConfig} from './tests/locked-items-web/config';
 import {config as objectWebConfig} from './tests/object-web/config';
+import {config as osbFaroWebConfig} from './tests/osb-faro-web/config';
 import {config as portalWebConfig} from './tests/portal-web/config';
 import {config as productNavigationUserPersonalBarWebConfig} from './tests/product-navigation-user-personal-bar-web/config';
 import {config as stableConfig} from './tests/stable/config';
@@ -29,6 +31,7 @@ export default defineConfig({
 	},
 	forbidOnly: !!process.env.CI,
 	projects: [
+		analyticsSettingsWebConfig,
 		batchPlannerConfig,
 		clientExtensionWebConfig,
 		commerceConfig,
@@ -42,6 +45,7 @@ export default defineConfig({
 		layoutContentPageEditorWebConfig,
 		lockedItemsConfig,
 		objectWebConfig,
+		osbFaroWebConfig,
 		portalWebConfig,
 		productNavigationUserPersonalBarWebConfig,
 		stableConfig,
