b2295e1222327e1d100a088cb45f341d1cbbac34
Marcos Martins <marcos.martins@liferay.com>
2024-02-05 10:20:05 -0300

    TEMP handle with ac on default scripts

--- modules/test/playwright/env/set_up.sh
+++ modules/test/playwright/env/set_up.sh
@@ -4,6 +4,10 @@ set -e -x
 
 source ${PLAYWRIGHT_BASE_DIR}/env/common.sh
 
+export FARO_URL=http://localhost:8081
+
+export PORTAL_URL=http://"$(hostname  -I | cut -f1 -d' ')":8080
+
 update_portal_ext_properties
 
 start_app_server
@@ -13,3 +17,5 @@ deploy_project_osgi_modules
 deploy_project_env_deploy_folder
 
 deploy_project_client_extensions
+
+start_ac
--- modules/test/playwright/env/tear_down.sh
+++ modules/test/playwright/env/tear_down.sh
@@ -5,3 +5,5 @@ set -e -x
 source ${PLAYWRIGHT_BASE_DIR}/env/common.sh
 
 stop_app_server
+
+stop_ac
