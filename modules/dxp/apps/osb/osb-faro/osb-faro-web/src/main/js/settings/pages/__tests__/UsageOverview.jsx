3db1963ee2f401f8d1503aae34aa23c9627e553d
Adriano Interaminense <adriano.interaminense@gmail.com>
2024-03-04 13:14:38 -0300

    LPD-19328 Rename it

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/__tests__/UsageOverview.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/__tests__/UsageOverview.jsx
@@ -98,7 +98,7 @@ describe('UsageOverview', () => {
 		).toBeInTheDocument();
 
 		expect(
-			getByText('104,000 Known Individuals remaining.')
+			getByText('104,000 Known Individuals available.')
 		).toBeInTheDocument();
 
 		expect(getByText('1,000 of 105,000 - 1% used.')).toBeInTheDocument();
@@ -153,7 +153,7 @@ describe('UsageOverview', () => {
 		).toBeInTheDocument();
 
 		expect(
-			getByText('6,888,877 Page Views remaining.')
+			getByText('6,888,877 Page Views available.')
 		).toBeInTheDocument();
 	});
 
