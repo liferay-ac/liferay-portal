dbd96b3745acda23011655023fedd9d6665f18ef
Marcellus Tavares <marcellus.tavares@liferay.com>
2024-04-26 12:12:51 -0700

    LPD-24471 Auto SF

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/constants/OSBAsahHeaderConstants.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/constants/OSBAsahHeaderConstants.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -10,13 +10,13 @@ package com.liferay.osb.faro.engine.client.constants;
  */
 public class OSBAsahHeaderConstants {
 
+	public static final String AUTHOR_USER_ID = "OSB-Asah-Author-User-ID";
+
+	public static final String AUTHOR_USER_NAME = "OSB-Asah-Author-User-Name";
+
 	public static final String FARO_BACKEND_SECURITY_SIGNATURE =
 		"OSB-Asah-Faro-Backend-Security-Signature";
 
 	public static final String PROJECT_ID = "OSB-Asah-Project-ID";
-
-	public static final String AUTHOR_USER_ID = "OSB-Asah-Author-User-ID";
-
-	public static final String AUTHOR_USER_NAME = "OSB-Asah-Author-User-Name";
 
 }
