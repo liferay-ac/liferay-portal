c5eac0e286655d7c940ca2765eb0e0a2bd4efbfd
Caio Elias Rabelo Pinheiro <caio.elias@liferay.com>
2025-01-20 14:22:50 -0300

    LPD-27117 updated test on edit.jsx

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/pages/__tests__/Edit.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/pages/__tests__/Edit.jsx
@@ -108,7 +108,7 @@ describe('Event Analysis Edit', () => {
 	});
 
 	it('should render event analysis with data', async () => {
-		const {container, getByText} = render(<WrappedComponent />);
+		const {container, getAllByText, getByText} = render(<WrappedComponent />);
 
 		jest.runAllTimers();
 
@@ -136,6 +136,7 @@ describe('Event Analysis Edit', () => {
 			container.querySelector('.compare-to-previous-checkbox input')
 				.checked
 		).toBeTruthy();
+		expect(getAllByText('displayName-0')).toBeTruthy(); 
 	});
 
 	it('should check if search autocomplete is working properly', async () => {
