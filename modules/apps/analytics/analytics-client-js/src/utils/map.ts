87c99c0f6eed175f5a1a7d040b27a7fb0c6ff77a
rafaellajordao <rafaella.jordao@liferay.com>
2025-02-25 16:34:27 -0300

    LPD-49257 Convert all javascript files into the src/utils dir

--- modules/apps/analytics/analytics-client-js/src/utils/map.ts
+++ modules/apps/analytics/analytics-client-js/src/utils/map.ts
@@ -3,16 +3,18 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-function convertMapToArr(mapInstance) {
-	const array = [];
+type MapInstance = [key: number, value: object][];
+
+function convertMapToArr(mapInstance: MapInstance) {
+	const array: [key: number, value: object][] = [];
 
 	mapInstance.forEach((value, key) => array.push([key, value]));
 
 	return array;
 }
 
-function getMapKeys(mapInstance) {
-	const array = [];
+function getMapKeys(mapInstance: MapInstance) {
+	const array: number[] = [];
 
 	mapInstance.forEach((value, key) => array.push(key));
 
