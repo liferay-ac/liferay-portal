48480e305ff79da4866890cf9d5dbc2f7e483b19
Tiago Campêlo <tiago.campelo@liferay.com>
2025-07-10 13:43:25 -0300

    LPD-59460 Refactor EmptyState

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/info_panel/tab_content/performance/EmptyState.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/info_panel/tab_content/performance/EmptyState.tsx
@@ -12,105 +12,84 @@ import {getImage} from '../../../../common/utils/getImage';
 import {EmptyStateData} from '../PerformanceTabContent';
 
 import '../../../../../css/infoPanel/PerfomanceTabEmptyState.scss';
-
 export function getEmptyState(data: EmptyStateData) {
+	let title = '';
+	let description = '';
+	let imgSrc = '';
+	let action = null;
+
 	if (!data.connectedToSpace) {
+		title = Liferay.Language.get('no-sites-are-connected-yet');
 		if (data.isAdmin) {
-			return (
-				<EmptyState
-					className="d-flex flex-column justify-content-center pt-6 text-center"
-					description={Liferay.Language.get(
-						'connect-sites-within-this-space'
-					)}
-					title={Liferay.Language.get('no-sites-are-connected-yet')}
-				>
-					<Button>{Liferay.Language.get('connect')}</Button>
-				</EmptyState>
+			description = Liferay.Language.get(
+				'connect-sites-within-this-space'
+			);
+			action = <Button>{Liferay.Language.get('connect')}</Button>;
+		}
+		else {
+			description = Liferay.Language.get(
+				'please-contact-an-administrator-to-sync-sites-to-this-space'
 			);
 		}
-
-		return (
-			<EmptyState
-				className="d-flex flex-column justify-content-center pt-6 text-center"
-				description={Liferay.Language.get(
-					'please-contact-an-administrator-to-sync-sites-to-this-space'
-				)}
-				title={Liferay.Language.get('no-sites-are-connected-yet')}
-			/>
-		);
 	}
-
-	if (!data.connectedToAnalyticsCloud) {
+	else if (!data.connectedToAnalyticsCloud) {
+		title = Liferay.Language.get('connect-to-liferay-analytics-cloud');
+		imgSrc = getImage('performance_tab_empty_state.svg');
 		if (data.isAdmin) {
-			return (
-				<EmptyState
-					className="d-flex flex-column justify-content-center pt-6 text-center"
-					description={Liferay.Language.get(
-						'in-order-to-view-asset-performance,-your-liferay-dxp-instance-has-to-be-connected-with-liferay-analytics-cloud'
-					)}
-					imgSrc={getImage('performance_tab_empty_state.svg')}
-					title={Liferay.Language.get(
-						'connect-to-liferay-analytics-cloud'
-					)}
+			description = Liferay.Language.get(
+				'in-order-to-view-asset-performance,-your-liferay-dxp-instance-has-to-be-connected-with-liferay-analytics-cloud'
+			);
+			action = (
+				<ClayLink
+					button
+					displayType="primary"
+					href={data.analyticsSettingsPortletURL}
 				>
-					<ClayLink
-						button
-						displayType="primary"
-						href={data.analyticsSettingsPortletURL}
-					>
-						{Liferay.Language.get('connect')}
-					</ClayLink>
-				</EmptyState>
+					{Liferay.Language.get('connect')}
+				</ClayLink>
 			);
 		}
-
-		return (
-			<EmptyState
-				className="d-flex flex-column justify-content-center pt-6 text-center"
-				description={Liferay.Language.get(
-					'please-contact-a-dxp-instance-administrator-to-connect-your-dxp-instance-to-analytics-cloud'
-				)}
-				imgSrc={getImage('performance_tab_empty_state.svg')}
-				title={Liferay.Language.get(
-					'connect-to-liferay-analytics-cloud'
-				)}
-			/>
-		);
-	}
-
-	if (!data.siteSyncedToAnalyticsCloud) {
-		if (data.isAdmin) {
-			return (
-				<EmptyState
-					className="d-flex flex-column justify-content-center pt-6 text-center"
-					description={Liferay.Language.get(
-						'in-order-to-view-asset-performance,-your-sites-have-to-be-synced-to-liferay-analytics-cloud'
-					)}
-					imgSrc={getImage('performance_tab_empty_state.svg')}
-					title={Liferay.Language.get('sync-to-analytics-cloud')}
-				>
-					<ClayLink
-						button
-						displayType="primary"
-						href={`${data.analyticsSettingsPortletURL}&currentPage=PROPERTIES`}
-					>
-						{Liferay.Language.get('sync')}
-					</ClayLink>
-				</EmptyState>
+		else {
+			description = Liferay.Language.get(
+				'please-contact-a-dxp-instance-administrator-to-connect-your-dxp-instance-to-analytics-cloud'
 			);
 		}
-
-		return (
-			<EmptyState
-				className="d-flex flex-column justify-content-center pt-6 text-center"
-				description={Liferay.Language.get(
-					'please-contact-a-dxp-instance-administrator-to-sync-your-sites-to-analytics-cloud'
-				)}
-				imgSrc={getImage('performance_tab_empty_state.svg')}
-				title={Liferay.Language.get('sync-to-analytics-cloud')}
-			/>
-		);
+	}
+	else if (!data.siteSyncedToAnalyticsCloud) {
+		title = Liferay.Language.get('sync-to-analytics-cloud');
+		imgSrc = getImage('performance_tab_empty_state.svg');
+		if (data.isAdmin) {
+			description = Liferay.Language.get(
+				'in-order-to-view-asset-performance,-your-sites-have-to-be-synced-to-liferay-analytics-cloud'
+			);
+			action = (
+				<ClayLink
+					button
+					displayType="primary"
+					href={`${data.analyticsSettingsPortletURL}&currentPage=PROPERTIES`}
+				>
+					{Liferay.Language.get('sync')}
+				</ClayLink>
+			);
+		}
+		else {
+			description = Liferay.Language.get(
+				'please-contact-a-dxp-instance-administrator-to-sync-your-sites-to-analytics-cloud'
+			);
+		}
+	}
+	else {
+		return null;
 	}
 
-	return null;
+	return (
+		<EmptyState
+			className="d-flex flex-column justify-content-center pt-6 text-center"
+			description={description}
+			imgSrc={imgSrc}
+			title={title}
+		>
+			{action}
+		</EmptyState>
+	);
 }
