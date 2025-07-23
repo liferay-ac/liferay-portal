37d0baa34a496361a1761cf115aef9d91b17416c
Thiago Buarque <thiago.buarque@liferay.com>
2025-07-23 17:12:43 -0300

    LPD-58597 send ExternalReferenceCode when rating an asset

--- modules/apps/ratings/ratings-taglib/src/main/java/com/liferay/ratings/taglib/servlet/taglib/RatingsTag.java
+++ modules/apps/ratings/ratings-taglib/src/main/java/com/liferay/ratings/taglib/servlet/taglib/RatingsTag.java
@@ -7,12 +7,17 @@ package com.liferay.ratings.taglib.servlet.taglib;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileEntryConstants;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.ExternalReferenceCodeModel;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.PersistedModel;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.PersistedModelLocalService;
+import com.liferay.portal.kernel.service.PersistedResourcedModelLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.trash.TrashHandlerRegistryUtil;
@@ -24,6 +29,7 @@ import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
+import com.liferay.portal.service.PersistedModelLocalServiceRegistryUtil;
 import com.liferay.ratings.kernel.RatingsType;
 import com.liferay.ratings.kernel.definition.PortletRatingsDefinitionUtil;
 import com.liferay.ratings.kernel.model.RatingsEntry;
@@ -35,6 +41,8 @@ import com.liferay.taglib.util.IncludeTag;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.jsp.PageContext;
+
+import java.util.List;
 
 /**
  * @author Ambrín Chaudhary
@@ -51,6 +59,58 @@ public class RatingsTag extends IncludeTag {
 
 	public String getContentTitle() {
 		return _contentTitle;
+	}
+
+	public String getExternalReferenceCode() {
+		if (Validator.isNotNull(_externalReferenceCode)) {
+			return _externalReferenceCode;
+		}
+
+		try {
+			PersistedModelLocalService persistedModelLocalService =
+				PersistedModelLocalServiceRegistryUtil.
+					getPersistedModelLocalService(_className);
+
+			PersistedModel model;
+
+			if (persistedModelLocalService instanceof
+					PersistedResourcedModelLocalService) {
+
+				PersistedResourcedModelLocalService
+					persistedResourcedModelLocalService =
+						(PersistedResourcedModelLocalService)
+							persistedModelLocalService;
+
+				List<? extends PersistedModel> persistedModels =
+					persistedResourcedModelLocalService.getPersistedModel(
+						_classPK);
+
+				model = persistedModels.get(0);
+			}
+			else {
+				model = persistedModelLocalService.getPersistedModel(
+					GetterUtil.getLong(_classPK));
+			}
+
+			if (model instanceof ExternalReferenceCodeModel) {
+				ExternalReferenceCodeModel externalReferenceCodeModel =
+					(ExternalReferenceCodeModel)model;
+
+				_externalReferenceCode =
+					externalReferenceCodeModel.getExternalReferenceCode();
+			}
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					StringBundler.concat(
+						"Unable to get external reference code for class name",
+						"\"", _className, "\" and class PK ", _classPK),
+					portalException);
+			}
+		}
+
+		return _externalReferenceCode;
 	}
 
 	public int getNumberOfStars() {
@@ -96,6 +156,10 @@ public class RatingsTag extends IncludeTag {
 		_contentTitle = contentTitle;
 	}
 
+	public void setExternalReferenceCode(String externalReferenceCode) {
+		_externalReferenceCode = externalReferenceCode;
+	}
+
 	public void setInTrash(boolean inTrash) {
 		_inTrash = inTrash;
 	}
@@ -138,6 +202,7 @@ public class RatingsTag extends IncludeTag {
 		_className = null;
 		_classPK = 0;
 		_contentTitle = null;
+		_externalReferenceCode = null;
 		_inTrash = null;
 		_numberOfStars = _NUMBER_OF_STARS;
 		_ratingsEntry = null;
@@ -160,6 +225,9 @@ public class RatingsTag extends IncludeTag {
 				"liferay-ratings:ratings:className", _className);
 			httpServletRequest.setAttribute(
 				"liferay-ratings:ratings:classPK", String.valueOf(_classPK));
+			httpServletRequest.setAttribute(
+				"liferay-ratings:ratings:externalReferenceCode",
+				getExternalReferenceCode());
 
 			boolean inTrash = _isInTrash();
 
@@ -197,6 +265,8 @@ public class RatingsTag extends IncludeTag {
 					"contentTitle", _contentTitle
 				).put(
 					"enabled", _isEnabled(themeDisplay, inTrash)
+				).put(
+					"externalReferenceCode", _externalReferenceCode
 				).put(
 					"initialAverageScore", _getInitialAverageScore(ratingsStats)
 				).put(
@@ -399,6 +469,7 @@ public class RatingsTag extends IncludeTag {
 	private String _className;
 	private long _classPK;
 	private String _contentTitle;
+	private String _externalReferenceCode;
 	private Boolean _inTrash;
 	private int _numberOfStars = _NUMBER_OF_STARS;
 	private RatingsEntry _ratingsEntry;
