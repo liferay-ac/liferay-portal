173d4468a3e984c8d04c0b90de7796b4a5279213
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2025-12-18 13:01:41 -0800

    LPD-74395 Append to file within image if it already exists

--- modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/resources/setenv.sh
+++ modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/resources/setenv.sh
@@ -1,6 +1,1 @@
-#!/bin/sh
-
-if [ -z "$CATALINA_OPTS" ]
-then
-    CLASSPATH=$CLASSPATH:$CATALINA_BASE/lib/ecs-logging-core-1.6.0.jar:$CATALINA_BASE/lib/jul-ecs-formatter-1.6.0.jar
-fi
+CLASSPATH=$CLASSPATH:$CATALINA_BASE/lib/ecs-logging-core-1.6.0.jar:$CATALINA_BASE/lib/jul-ecs-formatter-1.6.0.jar
