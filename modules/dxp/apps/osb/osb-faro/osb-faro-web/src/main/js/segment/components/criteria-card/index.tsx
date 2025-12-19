1e544cc43c885881f08b2d60eda6e94c3979e935
Rafaella Jordao <rafaella.jordao@liferay.com>
2025-12-19 15:55:16 -0300

    LPD-65944 Add ReportContainer to real-time segment cards

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/components/criteria-card/index.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/components/criteria-card/index.tsx
@@ -1,9 +1,11 @@
 import CriteriaView from './CriteriaView';
 import Label from 'shared/components/Label';
 import Panel from '@clayui/panel';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {ReportContainer} from 'shared/components/download-report/DownloadPDFReport';
 import {SegmentTypes} from 'shared/util/constants';
 import {translateQueryToCriteria} from 'segment/segment-editor/dynamic/utils/odata';
+import {useDownloadReportContext} from 'shared/components/download-report/DownloadReportContext';
 
 interface ICriteriaCardProps {
 	criteriaString: string;
@@ -20,6 +22,17 @@ const CriteriaCard: React.FC<ICriteriaCardProps> = ({
 }) => {
 	const _criteriaViewRef = React.createRef<HTMLDivElement>();
 
+	const {
+		clearReportContainers,
+		setReportContainer
+	} = useDownloadReportContext();
+
+	useEffect(() => {
+		setReportContainer(ReportContainer.SegmentCriteriaCard);
+
+		return clearReportContainers;
+	}, []);
+
 	return (
 		<Panel
 			className='card-root'
@@ -30,6 +43,7 @@ const CriteriaCard: React.FC<ICriteriaCardProps> = ({
 					{Liferay.Language.get('segment-criteria')}
 				</Panel.Title>
 			}
+			id={ReportContainer.SegmentCriteriaCard}
 		>
 			<Panel.Body className='criteria-card-root'>
 				{includeAnonymousUsers && (
