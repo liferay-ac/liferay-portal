38e172a3c07c5ae0558c4521af7d02b05f8da1b7
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2026-01-28 01:14:17 -0800

    LPD-76102 implement postSegment

--- modules/dxp/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/resource/v1_0/SegmentResourceImpl.java
+++ modules/dxp/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/resource/v1_0/SegmentResourceImpl.java
@@ -5,17 +5,47 @@
 
 package com.liferay.segments.asah.rest.internal.resource.v1_0;
 
+import com.liferay.segments.asah.connector.util.IndividualSegmentsHelper;
+import com.liferay.segments.asah.rest.dto.v1_0.Membership;
+import com.liferay.segments.asah.rest.dto.v1_0.Segment;
 import com.liferay.segments.asah.rest.resource.v1_0.SegmentResource;
+import com.liferay.segments.model.SegmentsEntry;
+
+import java.util.Arrays;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
- * @author Javier Gamarra
+ * @author Rachael Koestartyo
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/segment.properties",
 	scope = ServiceScope.PROTOTYPE, service = SegmentResource.class
 )
 public class SegmentResourceImpl extends BaseSegmentResourceImpl {
+
+	@Override
+	public Segment postSegment(String segmentsEntryKey, Segment segment)
+		throws Exception {
+
+		SegmentsEntry segmentsEntry =
+			_individualSegmentsHelper.addSegmentsEntry(
+				contextCompany.getCompanyId(), segmentsEntryKey,
+				segment.getName());
+
+		for (Membership membership : segment.getMemberships()) {
+			_individualSegmentsHelper.updateIndividualSegmentMemberships(
+				membership.getIndividualPK(),
+				Arrays.asList(membership.getIndividualSegmentIds()),
+				membership.getRemoved(), segmentsEntry, membership.getUserId());
+		}
+
+		return segment;
+	}
+
+	@Reference
+	private IndividualSegmentsHelper _individualSegmentsHelper;
+
 }
