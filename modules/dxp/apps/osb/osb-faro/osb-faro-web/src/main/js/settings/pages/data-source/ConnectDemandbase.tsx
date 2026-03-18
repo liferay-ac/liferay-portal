6541e1fc2af34caa55203f66efd234d228e14a17
Marcio Fonseca <marcio.fonseca@liferay.com>
2026-03-18 16:38:16 -0300

    LPD-83028 and LPD-83032 create page and component for ConnectDemandbase

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/ConnectDemandbase.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import WizardPage, {Step} from 'settings/components/base-page/WizardPage';
+import {Alert} from 'shared/types';
+import {AssignIndividualsDataToPropertiesStep} from 'settings/components/salesforce/steps/AssignIndividualsDataToChannelsStep';
+import {ConnectDemandbaseStep} from 'settings/components/demandbase/steps/ConnectDemandbaseStep';
+import {updateSalesforce} from 'shared/api/data-source';
+
+const steps: Step[] = [
+	{
+		content: props => <ConnectDemandbaseStep {...props} />,
+		description: Liferay.Language.get(
+			'connect-your-demandbase-account-to-analytics-cloud-to-start-importing-company-people-and-engagement-data'
+		),
+		title: Liferay.Language.get('connect-demandbase')
+	},
+
+	{
+		content: props => (
+			<AssignIndividualsDataToPropertiesStep
+				{...props}
+				onSubmit={dataSource => {
+					const accountsEnabled = dataSource.provider.getIn([
+						'accountsConfiguration',
+						'enableAllAccounts'
+					]);
+
+					const contactsConfiguration = dataSource.provider.get(
+						'contactsConfiguration'
+					);
+
+					const individualsEnabled =
+						contactsConfiguration.get('enableAllContacts') &&
+						contactsConfiguration.get('enableAllLeads');
+
+					if (accountsEnabled || individualsEnabled) {
+						props.addAlert({
+							alertType: Alert.Types.Success,
+							message: Liferay.Language.get(
+								'the-data-source-setup-is-now-complete,-and-you-will-begin-to-see-data-as-activities-occur-on-your-sites'
+							)
+						});
+					} else {
+						props.addAlert({
+							alertType: Alert.Types.Success,
+							message: Liferay.Language.get(
+								'the-data-source-setup-has-finished'
+							)
+						});
+					}
+				}}
+				updateDataSourceFn={updateSalesforce}
+			/>
+		),
+		description: Liferay.Language.get(
+			'properties-allow-you-to-aggregate-data-on-your-users,-sites-and-dxp-commerce-channels.-individuals-data-will-be-available-in-any-property-they-are-assigned-to'
+		),
+		title: Liferay.Language.get('assign-individuals-data-to-properties')
+	}
+];
+
+const ConnectDemandbase = () => <WizardPage steps={steps} />;
+
+export default ConnectDemandbase;
