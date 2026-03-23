c234b7ad0a7b8caf16ed7071bdf0af0ef3656678
Adriano Interaminense <adriano.interaminense@gmail.com>
2026-03-23 14:19:43 -0300

    Revert "LPD-82000 Backport missing props to v4.15.x"
    This reverts commit af1e9842c41ae419616b53bbabdda0fc9c42b1fa.

--- modules/apps/frontend-js/frontend-js-importmaps-extender/src/main/java/com/liferay/frontend/js/importmaps/extender/internal/servlet/taglib/JSImportMapsExtenderTopHeadDynamicInclude.java
+++ modules/apps/frontend-js/frontend-js-importmaps-extender/src/main/java/com/liferay/frontend/js/importmaps/extender/internal/servlet/taglib/JSImportMapsExtenderTopHeadDynamicInclude.java
@@ -102,7 +102,7 @@ public class JSImportMapsExtenderTopHeadDynamicInclude
 
 	@Override
 	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
-		dynamicIncludeRegistry.register("importmap");
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
 	}
 
 	@Activate
