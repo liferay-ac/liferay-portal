a5341afd72900f2cb90c05fb96a53133ecd87e59
Adriano Interaminense <adriano.interaminense@gmail.com>
2026-03-24 17:33:45 -0300

    LPD-82000 Include Dropdown Range Key on header

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/assets/List.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/assets/List.tsx
@@ -2,12 +2,16 @@ import * as breadcrumbs from 'shared/util/breadcrumbs';
 import BasePage from 'shared/components/base-page';
 import Card from 'shared/components/Card';
 import ClayLink from '@clayui/link';
-import React from 'react';
+import React, {useState} from 'react';
+import {DropdownRangeKey} from 'shared/components/dropdown-range-key/DropdownRangeKey';
+import {RangeSelectors} from 'shared/types';
 import {Routes, toRoute} from 'shared/util/router';
 import {toThousands} from 'shared/util/numbers';
 import {useChannelContext} from 'shared/context/channel';
 import {useFrontendDataSet} from 'shared/hooks/useFrontendDataSet';
 import {useParams} from 'react-router-dom';
+import {useQueryRangeSelectors} from 'shared/hooks/useQueryRangeSelectors';
+import {pagination} from 'shared/util/frontend-data-set';
 
 const mapRoutes = {
 	blog: Routes.ASSETS_BLOGS_OVERVIEW,
@@ -19,8 +23,23 @@ const mapRoutes = {
 const List = () => {
 	const {selectedChannel} = useChannelContext();
 	const {channelId, groupId} = useParams();
+	const initialRangeSelectors = useQueryRangeSelectors();
+
+	const [rangeSelectors, setRangeSelectors] = useState<RangeSelectors>(
+		initialRangeSelectors
+	);
 
 	const FrontendDataSet = useFrontendDataSet();
+
+	let apiURL = `/o/faro/contacts/${groupId}/asset-summary?channelId=${channelId}&rangeKey=${rangeSelectors.rangeKey}`;
+
+	if (rangeSelectors.rangeEnd) {
+		apiURL += `&rangeEnd=${rangeSelectors.rangeEnd}`;
+	}
+
+	if (rangeSelectors.rangeStart) {
+		apiURL += `&rangeStart=${rangeSelectors.rangeStart}`;
+	}
 
 	return (
 		<BasePage documentTitle={Liferay.Language.get('assets')}>
@@ -38,11 +57,22 @@ const List = () => {
 					title={Liferay.Language.get('assets')}
 				/>
 			</BasePage.Header>
+
+			<BasePage.SubHeader>
+				<div className='d-flex justify-content-end w-100'>
+					<DropdownRangeKey
+						legacy={false}
+						onRangeSelectorChange={setRangeSelectors}
+						rangeSelectors={rangeSelectors}
+					/>
+				</div>
+			</BasePage.SubHeader>
+
 			<BasePage.Body>
 				<Card>
 					{FrontendDataSet && (
 						<FrontendDataSet
-							apiURL={`/o/faro/contacts/${groupId}/asset-summary?channelId=${channelId}`}
+							apiURL={apiURL}
 							customDataRenderers={{
 								assetMetricRenderer: ({value}) => (
 									<span>{toThousands(value.value)}</span>
@@ -78,44 +108,8 @@ const List = () => {
 									return <span>{value || itemData.id}</span>;
 								}
 							}}
-							pagination={{
-								deltas: [{label: 10}, {label: 20}, {label: 50}],
-								initialDelta: 20,
-								initialPageNumber: 1
-							}}
+							pagination={pagination}
 							showPagination
-							sort={[
-								{
-									active: true,
-									direction: 'asc',
-									key: 'assetId',
-									label: Liferay.Language.get('title')
-								},
-								{
-									active: false,
-									direction: 'asc',
-									key: 'assetType',
-									label: Liferay.Language.get('type')
-								},
-								{
-									active: false,
-									direction: 'desc',
-									key: 'viewsMetric',
-									label: Liferay.Language.get('views')
-								},
-								{
-									active: false,
-									direction: 'desc',
-									key: 'impressionsMetric',
-									label: Liferay.Language.get('impressions')
-								},
-								{
-									active: false,
-									direction: 'desc',
-									key: 'downloadsMetric',
-									label: Liferay.Language.get('downloads')
-								}
-							]}
 							views={[
 								{
 									contentRenderer: 'table',
