b6e8f115694fe4ca77c4b425a7aa71ad1d02d6ce
Tiago Campêlo <tiago.campelo@liferay.com>
2026-04-09 10:32:59 -0300

    LPD-85337 Create Lifecycle Context

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/lifecycle/pages/BaseLifecycle.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/lifecycle/pages/BaseLifecycle.tsx
@@ -1,7 +1,9 @@
 import * as breadcrumbs from 'shared/util/breadcrumbs';
 import BasePage from 'shared/components/base-page';
+import GlobalFilters from '../components/GlobalFilters';
 import React, {useContext} from 'react';
 import {ChannelContext} from 'shared/context/channel';
+import {LifecycleContextProvider} from '../context/LifecycleContext';
 import {useParams} from 'react-router-dom';
 
 const BaseLifecycle = () => {
@@ -10,28 +12,32 @@ const BaseLifecycle = () => {
 	const {channelId, groupId} = useParams();
 
 	return (
-		<BasePage documentTitle={Liferay.Language.get('lifecycles')}>
-			<BasePage.Header
-				breadcrumbs={[
-					breadcrumbs.getHome({
-						channelId,
-						groupId,
-						label: selectedChannel.name
-					})
-				]}
-				groupId={groupId}
-			>
-				<BasePage.Row>
-					<BasePage.Header.TitleSection
-						className='mb-3'
-						title={Liferay.Language.get('lifecycles')}
-					/>
-				</BasePage.Row>
-			</BasePage.Header>
-			<BasePage.SubHeader>
-				<div className='d-flex justify-content-between w-100'></div>
-			</BasePage.SubHeader>
-		</BasePage>
+		<LifecycleContextProvider>
+			<BasePage documentTitle={Liferay.Language.get('lifecycles')}>
+				<BasePage.Header
+					breadcrumbs={[
+						breadcrumbs.getHome({
+							channelId,
+							groupId,
+							label: selectedChannel.name
+						})
+					]}
+					groupId={groupId}
+				>
+					<BasePage.Row>
+						<BasePage.Header.TitleSection
+							className='mb-3'
+							title={Liferay.Language.get('lifecycles')}
+						/>
+					</BasePage.Row>
+				</BasePage.Header>
+				<BasePage.SubHeader>
+					<div className='d-flex justify-content-between w-100'>
+						<GlobalFilters />
+					</div>
+				</BasePage.SubHeader>
+			</BasePage>
+		</LifecycleContextProvider>
 	);
 };
 
