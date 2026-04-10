74e52884283fcbd68b26b18aab84d7491bf7b5ea
Thiago Buarque <thiago.buarque@liferay.com>
2026-04-10 17:48:06 -0300

    LPD-84494 adapt to frontend dataset pattern

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
@@ -2588,7 +2588,7 @@ public class ContactsEngineClientImpl
 	@Override
 	public Results<PageExperience> getPageExperiences(
 			FaroProject faroProject, String canonicalUrl, String channelId,
-			String keywords, int page, String pageTitle, int size,
+			int page, String pageTitle, String search, int size,
 			String sortString)
 		throws Exception {
 
@@ -2598,8 +2598,8 @@ public class ContactsEngineClientImpl
 		uriVariables.put("canonicalUrl", canonicalUrl);
 		uriVariables.put("channelId", channelId);
 
-		if (Validator.isNotNull(keywords)) {
-			uriVariables.put("keywords", keywords);
+		if (Validator.isNotNull(search)) {
+			uriVariables.put("keywords", search);
 		}
 
 		uriVariables.put("pageTitle", pageTitle);
