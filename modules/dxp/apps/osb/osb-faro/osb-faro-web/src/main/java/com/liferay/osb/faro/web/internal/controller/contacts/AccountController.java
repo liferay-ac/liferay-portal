a7419b46b07f6a42c17ec0ace0b957dd130b7db6
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2026-04-10 11:20:20 -0700

    LPD-83754 Add endpoint to return account field values in FDS accepted format

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/contacts/AccountController.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/contacts/AccountController.java
@@ -30,6 +30,7 @@ import jakarta.ws.rs.core.MediaType;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.osgi.service.component.annotations.Component;
