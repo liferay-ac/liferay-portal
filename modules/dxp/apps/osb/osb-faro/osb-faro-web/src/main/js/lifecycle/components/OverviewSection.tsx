5f2148ee8ec8b07070fdf78975cf391ce9704435
Tiago Campêlo <tiago.campelo@liferay.com>
2026-04-17 23:48:26 -0300

    LPD-84340 Refactor Overview Section

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/lifecycle/components/OverviewSection.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/lifecycle/components/OverviewSection.tsx
@@ -5,12 +5,12 @@ import {SectionHeader} from 'individual/profile/components/SectionHeader';
 import {sub} from 'shared/util/lang';
 
 interface IOverviewSectionProps {
-	metrics: {
+	loading?: boolean;
+	metrics?: {
 		atRiskAccounts?: Metric;
 		progressedAccounts?: Metric;
 		staticAccounts?: Metric;
 	};
-	loading?: boolean;
 }
 
 const renderTrendLabel = (percentageNode: React.ReactNode) =>
@@ -21,11 +21,35 @@ const OverviewSection: React.FC<IOverviewSectionProps> = ({
 	metrics
 }) => {
 	const atRiskAccounts = metrics?.atRiskAccounts;
-	const progressedAccounts = metrics?.progressedAccounts;
-	const staticAccounts = metrics?.staticAccounts;
+	const netNewPipelineGenerated = metrics?.progressedAccounts;
+	const stalledAccounts = metrics?.staticAccounts;
 
 	const cardContainerClassName = 'col-12 col-lg-4 d-flex';
 	const bodyClassName = 'd-flex flex-column justify-content-around';
+
+	const cards = [
+		{
+			description: Liferay.Language.get(
+				'total-new-accounts-that-entered-the-pipeline-stage-within-the-selected-timeframe,-excluding-cross-sells,-upsells,-and-renewals.'
+			),
+			metric: netNewPipelineGenerated,
+			title: Liferay.Language.get('net-new-pipeline-generated')
+		},
+		{
+			description: Liferay.Language.get(
+				'the-total-number-of-accounts-specifically-stuck-in-the-engaged-stage-that-have-exceeded-their-designated-time-in-stage-threshold-(>-90-days)'
+			),
+			metric: stalledAccounts,
+			title: Liferay.Language.get('stalled-accounts')
+		},
+		{
+			description: Liferay.Language.get(
+				'customers-showing-a-drop-in-product-usage-or-warning-signs-of-churn.-requires-immediate-intervention.'
+			),
+			metric: atRiskAccounts,
+			title: Liferay.Language.get('at-risk-accounts')
+		}
+	];
 
 	return (
 		<>
@@ -34,50 +58,20 @@ const OverviewSection: React.FC<IOverviewSectionProps> = ({
 				title={Liferay.Language.get('overview')}
 			/>
 			<div className='row g-4'>
-				<div className={cardContainerClassName}>
-					<MetricCard
-						bodyClassName={bodyClassName}
-						description={Liferay.Language.get(
-							'total-new-accounts-that-entered-the-pipeline-stage-within-the-selected-timeframe,-excluding-cross-sells,-upsells,-and-renewals.'
-						)}
-						loading={loading}
-						renderTrendLabel={renderTrendLabel}
-						title={Liferay.Language.get(
-							'net-new-pipeline-generated'
-						)}
-						trend={progressedAccounts?.trend}
-						trendClassName='text-lowercase'
-						value={progressedAccounts?.value ?? 0}
-					/>
-				</div>
-				<div className={cardContainerClassName}>
-					<MetricCard
-						bodyClassName={bodyClassName}
-						description={Liferay.Language.get(
-							'the-total-number-of-accounts-specifically-stuck-in-the-engaged-stage-that-have-exceeded-their-designated-time-in-stage-threshold-(>-90-days)'
-						)}
-						loading={loading}
-						renderTrendLabel={renderTrendLabel}
-						title={Liferay.Language.get('stalled-accounts')}
-						trend={staticAccounts?.trend}
-						trendClassName='text-lowercase'
-						value={staticAccounts?.value ?? 0}
-					/>
-				</div>
-				<div className={cardContainerClassName}>
-					<MetricCard
-						bodyClassName={bodyClassName}
-						description={Liferay.Language.get(
-							'customers-showing-a-drop-in-product-usage-or-warning-signs-of-churn.-requires-immediate-intervention.'
-						)}
-						loading={loading}
-						renderTrendLabel={renderTrendLabel}
-						title={Liferay.Language.get('at-risk-accounts')}
-						trend={atRiskAccounts?.trend}
-						trendClassName='text-lowercase'
-						value={atRiskAccounts?.value ?? 0}
-					/>
-				</div>
+				{cards.map(({description, metric, title}) => (
+					<div className={cardContainerClassName} key={title}>
+						<MetricCard
+							bodyClassName={bodyClassName}
+							description={description}
+							loading={loading}
+							renderTrendLabel={renderTrendLabel}
+							title={title}
+							trend={metric?.trend}
+							trendClassName='text-lowercase'
+							value={metric?.value ?? 0}
+						/>
+					</div>
+				))}
 			</div>
 		</>
 	);
