14ec59e6b29bc943c04452b822f5ee0e6dcc499b
Tiago Campêlo <tiago.campelo@liferay.com>
2026-04-22 21:41:02 -0300

    LPD-84340 Create Lifecycle Overview with filters

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/api/index.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/api/index.js
@@ -14,6 +14,7 @@ import * as individuals from './individuals';
 import * as individualSegment from './individual-segment';
 import * as interests from './interests';
 import * as issue from './issue';
+import * as lifecycle from './lifecycle';
 import * as notifications from './notifications';
 import * as pagesVisited from './pages-visited';
 import * as preferences from './preferences';
@@ -38,6 +39,7 @@ export {
 	individuals,
 	interests,
 	issue,
+	lifecycle,
 	notifications,
 	pagesVisited,
 	preferences,
