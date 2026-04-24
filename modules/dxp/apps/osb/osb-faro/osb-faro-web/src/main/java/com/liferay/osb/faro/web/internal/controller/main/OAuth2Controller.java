f0b1807e4b4e6c41e1264e8cab62f997af24c1bf
Nilton Vieira <nilton.vieira@liferay.com>
2026-04-24 11:16:15 -0300

    LPD-87407 create tokens based on hubspot type

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/OAuth2Controller.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/controller/main/OAuth2Controller.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.SecureRandomUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -51,6 +52,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -288,17 +290,11 @@ public class OAuth2Controller extends BaseFaroController {
 
 		ClientProfile clientProfile = ClientProfile.HEADLESS_SERVER;
 
-		List<String> scopes;
-
-		if (Validator.isNotNull(type)) {
-			scopes = Arrays.asList(
-				ApiApplication.OAuth2ScopeAliases.ACCOUNTS_WRITE);
-		}
-		else {
-			scopes = Arrays.asList(
+		List<String> scopes = _oAuth2ScopesMap.getOrDefault(
+			type,
+			Arrays.asList(
 				ApiApplication.OAuth2ScopeAliases.RECOMMENDATIONS_EVERYTHING,
-				ApiApplication.OAuth2ScopeAliases.REPORTS_EVERYTHING);
-		}
+				ApiApplication.OAuth2ScopeAliases.REPORTS_EVERYTHING));
 
 		return _oAuth2ApplicationLocalService.addOAuth2Application(
 			user.getCompanyId(), user.getUserId(), user.getFullName(),
@@ -409,5 +405,14 @@ public class OAuth2Controller extends BaseFaroController {
 
 	@Reference
 	private OAuth2AuthorizationService _oAuth2AuthorizationService;
+
+	private final Map<String, List<String>> _oAuth2ScopesMap =
+		HashMapBuilder.<String, List<String>>put(
+			"demandbase",
+			Arrays.asList(ApiApplication.OAuth2ScopeAliases.ACCOUNTS_WRITE)
+		).put(
+			"hubspot",
+			Arrays.asList(ApiApplication.OAuth2ScopeAliases.HUBSPOT_WRITE)
+		).build();
 
 }
