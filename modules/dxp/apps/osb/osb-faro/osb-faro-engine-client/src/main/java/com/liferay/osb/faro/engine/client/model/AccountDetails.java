451bbb913fcc2fd621cd1774adb3141f85a54a2c
Caio Pinheiro <caio.elias@liferay.com>
2026-05-04 16:27:26 -0300

    LPD-85376 Simplify AccountDetails and AccountDetailsDisplay to only expose fields list

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/model/AccountDetails.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/model/AccountDetails.java
@@ -5,7 +5,6 @@
 
 package com.liferay.osb.faro.engine.client.model;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -13,134 +12,12 @@ import java.util.List;
  */
 public class AccountDetails {
 
-	public String getAccountName() {
-		return _accountName;
-	}
-
-	public String getAccountType() {
-		return _accountType;
-	}
-
-	public Double getAnnualRevenue() {
-		return _annualRevenue;
-	}
-
-	public String getCountry() {
-		return _country;
-	}
-
-	public Date getCreateDate() {
-		if (_createDate == null) {
-			return null;
-		}
-
-		return new Date(_createDate.getTime());
-	}
-
-	public String getCurrencyCode() {
-		return _currencyCode;
-	}
-
 	public List<Field> getFields() {
 		return _fields;
 	}
 
-	public String getId() {
-		return _id;
-	}
-
-	public String getIndustry() {
-		return _industry;
-	}
-
-	public Date getLastActivityDate() {
-		if (_lastActivityDate == null) {
-			return null;
-		}
-
-		return new Date(_lastActivityDate.getTime());
-	}
-
-	public Date getModifiedDate() {
-		if (_modifiedDate == null) {
-			return null;
-		}
-
-		return new Date(_modifiedDate.getTime());
-	}
-
-	public Integer getNumberOfEmployees() {
-		return _numberOfEmployees;
-	}
-
-	public String getState() {
-		return _state;
-	}
-
-	public String getYearStarted() {
-		return _yearStarted;
-	}
-
-	public void setAccountName(String accountName) {
-		_accountName = accountName;
-	}
-
-	public void setAccountType(String accountType) {
-		_accountType = accountType;
-	}
-
-	public void setAnnualRevenue(Double annualRevenue) {
-		_annualRevenue = annualRevenue;
-	}
-
-	public void setCountry(String country) {
-		_country = country;
-	}
-
-	public void setCreateDate(Date createDate) {
-		if (createDate != null) {
-			_createDate = new Date(createDate.getTime());
-		}
-	}
-
-	public void setCurrencyCode(String currencyCode) {
-		_currencyCode = currencyCode;
-	}
-
 	public void setFields(List<Field> fields) {
 		_fields = fields;
-	}
-
-	public void setId(String id) {
-		_id = id;
-	}
-
-	public void setIndustry(String industry) {
-		_industry = industry;
-	}
-
-	public void setLastActivityDate(Date lastActivityDate) {
-		if (lastActivityDate != null) {
-			_lastActivityDate = new Date(lastActivityDate.getTime());
-		}
-	}
-
-	public void setModifiedDate(Date modifiedDate) {
-		if (modifiedDate != null) {
-			_modifiedDate = new Date(modifiedDate.getTime());
-		}
-	}
-
-	public void setNumberOfEmployees(Integer numberOfEmployees) {
-		_numberOfEmployees = numberOfEmployees;
-	}
-
-	public void setState(String state) {
-		_state = state;
-	}
-
-	public void setYearStarted(String yearStarted) {
-		_yearStarted = yearStarted;
 	}
 
 	public static class Field {
@@ -175,19 +52,6 @@ public class AccountDetails {
 
 	}
 
-	private String _accountName;
-	private String _accountType;
-	private Double _annualRevenue;
-	private String _country;
-	private Date _createDate;
-	private String _currencyCode;
 	private List<Field> _fields;
-	private String _id;
-	private String _industry;
-	private Date _lastActivityDate;
-	private Date _modifiedDate;
-	private Integer _numberOfEmployees;
-	private String _state;
-	private String _yearStarted;
 
 }
--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/model/display/contacts/AccountDetailsDisplay.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/java/com/liferay/osb/faro/web/internal/model/display/contacts/AccountDetailsDisplay.java
@@ -5,11 +5,8 @@
 
 package com.liferay.osb.faro.web.internal.model.display.contacts;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import com.liferay.osb.faro.engine.client.model.AccountDetails;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,40 +18,13 @@ public class AccountDetailsDisplay {
 	}
 
 	public AccountDetailsDisplay(AccountDetails accountDetails) {
-		_accountName = accountDetails.getAccountName();
-		_accountType = accountDetails.getAccountType();
-		_annualRevenue = accountDetails.getAnnualRevenue();
-		_country = accountDetails.getCountry();
-		_createDate = accountDetails.getCreateDate();
-		_currencyCode = accountDetails.getCurrencyCode();
 		_fields = accountDetails.getFields();
-		_id = accountDetails.getId();
-		_industry = accountDetails.getIndustry();
-		_lastActivityDate = accountDetails.getLastActivityDate();
-		_modifiedDate = accountDetails.getModifiedDate();
-		_numberOfEmployees = accountDetails.getNumberOfEmployees();
-		_state = accountDetails.getState();
-		_yearStarted = accountDetails.getYearStarted();
 	}
 
-	private String _accountName;
-	private String _accountType;
-	private Double _annualRevenue;
-	private String _country;
-	private Date _createDate;
-	private String _currencyCode;
+	public List<AccountDetails.Field> getFields() {
+		return _fields;
+	}
+
 	private List<AccountDetails.Field> _fields;
-	private String _id;
-	private String _industry;
-
-	@JsonProperty("lastActive")
-	private Date _lastActivityDate;
-
-	@JsonProperty("lastEnriched")
-	private Date _modifiedDate;
-
-	private Integer _numberOfEmployees;
-	private String _state;
-	private String _yearStarted;
 
 }
