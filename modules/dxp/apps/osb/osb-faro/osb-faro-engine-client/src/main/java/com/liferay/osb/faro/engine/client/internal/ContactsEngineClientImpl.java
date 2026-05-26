e282655ef6960d2e600cdaa6192d26bd776b6ea3
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2026-05-26 13:02:07 +0200

    LPD-91080 osb-faro-engine-client Wire Demandbase count to new endpoint

--- modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-engine-client/src/main/java/com/liferay/osb/faro/engine/client/internal/ContactsEngineClientImpl.java
@@ -1812,12 +1812,11 @@ public class ContactsEngineClientImpl
 
 		Map<String, Object> uriVariables = getUriVariables(faroProject);
 
-		if (!Validator.isBlank(dataSourceId)) {
-			uriVariables.put("dataSourceId", dataSourceId);
-		}
+		uriVariables.put("dataSourceId", dataSourceId);
 
 		ResponseEntity<Long> responseEntity = restTemplate.exchange(
-			getTemplatedURL(faroProject, Rels.DEMANDBASE_ACCOUNTS_COUNT),
+			getTemplatedURL(
+				faroProject, Rels.DATA_SOURCE_METRICS_ACCOUNTS_COUNT),
 			HttpMethod.GET, HttpEntity.EMPTY, Long.class, uriVariables);
 
 		if (responseEntity.getBody() == null) {
