17e1b851f1fade79399f1a07f567b5506e65a374
Rafaella Jordao <rafaella.jordao@liferay.com>
2026-09-08 19:00:20 -0300

    LPD-99052 Label pages served by a non-default page experience

--- workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/shared/queries/UserSessionQuery.ts
+++ workspaces/liferay-osbfaro-workspace/modules/osb-faro-web/src/main/js/shared/queries/UserSessionQuery.ts
@@ -10,6 +10,8 @@ export interface UserSessionEvent {
 	createDate: string;
 	eventDate: string;
 	eventId: string;
+	experienceId?: string | null;
+	experienceName?: string | null;
 	name: string;
 	pageDescription: string;
 	pageGroupId?: string | null;
@@ -99,6 +101,8 @@ export default gql`
 						createDate
 						eventDate
 						eventId
+						experienceId
+						experienceName
 						name
 						pageDescription
 						pageGroupId
